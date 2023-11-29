4e6dc953ec0a1ca1753cd91f8a91781d82bb94b0
tubo213 <yuuki.32040808@gmail.com>
2023-11-30 00:53:28 +0900

    dictを再帰的に展開してcsvで保存

--- tools/export_result.py
+++ tools/export_result.py
@@ -5,6 +5,19 @@ import pandas as pd
 import wandb
 
 
+# 再帰的にdictを展開する
+def flatten_dict(d, parent_key="", sep="_"):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
 @click.command()
 @click.option("--user_name", type=str, default="213tubo", help="wandb user name")
 @click.option("--output_dir", type=Path, default="./output", help="output directory")
@@ -14,24 +27,28 @@ def export_result(user_name: str, output_dir: Path):
     # Project is specified by <entity/project-name>
     runs = api.runs(f"{user_name}/nsw-with-optimal-transport")
 
-    summary_list, config_list, name_list = [], [], []
+    data_list = []
     for run in runs:
         # .summary contains the output keys/values for metrics like accuracy.
         #  We call ._json_dict to omit large files
-        summary_list.append(run.summary._json_dict)
+        data = flatten_dict(run.summary._json_dict)
 
         # .config contains the hyperparameters.
-        #  We remove special values that start with _.
-        config_list.append({k: v for k, v in run.config.items() if not k.startswith("_")})
+        # run.configをflattenする
+        config = flatten_dict(run.config)
+        for k, v in config.items():
+            data[k] = v
 
         # .name is the human-readable name of the run.
-        name_list.append(run.name)
+        data["name"] = run.name
 
-    runs_df = pd.DataFrame({"summary": summary_list, "config": config_list, "name": name_list})
+        data_list.append(data)
 
+    runs_df = pd.DataFrame(data_list)
     output_dir = Path("./output")
     output_dir.mkdir(exist_ok=True, parents=True)
     runs_df.to_csv(output_dir / "result.csv")
+
 
 if __name__ == "__main__":
     export_result()
