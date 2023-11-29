76b6976038a6345fda7e014f9e676f5d97cf0703
tubo213 <yuuki.32040808@gmail.com>
2023-11-29 23:48:03 +0900

    log with wandb

--- run/main.py
+++ run/main.py
@@ -1,6 +1,8 @@
 from time import time
 
 import hydra
+import omegaconf
+import wandb
 from pytorch_lightning import seed_everything
 
 from src.conf import Config
@@ -11,6 +13,9 @@ from src.optimizer.common import get_optimizer
 
 @hydra.main(config_path="conf", config_name="main", version_base="1.2")
 def main(cfg: Config):
+    wandb_config = omegaconf.OmegaConf.to_container(cfg, resolve=True, throw_on_missing=True)
+    wandb.init(project="nsw-with-optimal-transport", name=cfg.exp_name, config=wandb_config)  # type: ignore
+
     seed_everything(cfg.seed)
     generator_cfg = cfg.generator
     rel_mat_true, rel_mat_obs = synthesize_rel_mat(
@@ -32,7 +37,16 @@ def main(cfg: Config):
     result.exec_time = exec_time
 
     print(result)
-    return result
+
+    wandb.log(
+        {
+            "user_util": result.user_util,
+            "item_utils": result.item_utils,
+            "max_envies": result.max_envies,
+            "nsw": result.nsw,
+            "exec_time": result.exec_time,
+        }
+    )
 
 
 if __name__ == "__main__":
