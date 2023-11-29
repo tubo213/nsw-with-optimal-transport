60b18a1d8f2a0b55e371e33ab70a16bdc0650cb4
tubo213 <yuuki.32040808@gmail.com>
2023-11-30 04:01:36 +0900

    add ref

--- src/evaluator.py
+++ src/evaluator.py
@@ -12,7 +12,7 @@ class Result:
     nsw: float
     exec_time: Optional[float] = None
 
-
+# ref: https://github.com/usaito/kdd2022-fair-ranking-nsw
 def evaluate_pi(pi: np.ndarray, rel_mat: np.ndarray, v: np.ndarray) -> Result:
     n_query, n_doc = rel_mat.shape
     expo_mat = (pi * v.T).sum(2)
