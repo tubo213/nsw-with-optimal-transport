342f93ae548b14b13bf196dce5570712c4b02767
tubo213 <yuuki.32040808@gmail.com>
2024-04-09 21:49:33 +0900

    fix bug

--- src/optimizer/greedy.py
+++ src/optimizer/greedy.py
@@ -3,10 +3,29 @@ import numpy as np
 from src.optimizer.base import BaseOptimizer
 
 
+def choice_maximize_nsw_doc(
+    impact_on_items: np.ndarray,  # (n_doc)
+    impact_qk: np.ndarray,  # (n_doc)
+    used_docs: np.ndarray,  # (n_doc)
+    eps: float = 1e-5,
+) -> int:
+    # 既に選択されたdocの影響は0
+    impact_qk = impact_qk * ~used_docs  # (n_doc)
+
+    # 各docを選択した場合の影響を計算
+    impact_qk_diag = np.diag(impact_qk)  # (n_doc, n_doc)
+    this_impact_on_items = impact_on_items[:, None] + impact_qk_diag  # (n_doc, n_doc)
+
+    # nswの目的関数値が一番大きくなるdocを選択
+    ln_nsw = np.log(this_impact_on_items + eps).sum(0)  # (n_doc)
+    best_doc = np.argmax(ln_nsw)
+
+    return best_doc
+
+
 def compute_greedy(
     rel_mat: np.ndarray,  # (n_query, n_doc)
     expo: np.ndarray,  # (K, 1)
-    eps: float = 1e-5,
 ) -> np.ndarray:
     n_query, n_doc = rel_mat.shape
     K = expo.shape[0]
@@ -17,11 +36,11 @@ def compute_greedy(
 
     for k in range(K):
         for q in range(n_query):
-            # nswの目的関数値が一番大きくなるdocを選ぶ
-            impact_qk = impact[q, :, k] * ~used_docs[q]  # (n_doc)
-            this_impact_on_items = impact_on_items[:, None] + impact_qk[None, :]  # (n_doc, n_doc)
-            ln_nsw = np.log(this_impact_on_items + eps).sum(0)  # (n_doc)
-            best_doc = np.argmax(ln_nsw)
+            impact_qk = impact[q, :, k]  # (n_doc)
+            # 目的関数が最も大きくなるdocを選択
+            best_doc = choice_maximize_nsw_doc(impact_on_items, impact_qk, used_docs[q])
+
+            # 更新
             impact_on_items[best_doc] += impact_qk[best_doc]
             pi[q, best_doc, k] = 1
             used_docs[q, best_doc] = True
