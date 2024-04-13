74a0f0858ca94c1d1c795b9772f97b0d91a5a3d7
tubo213 <yuuki.32040808@gmail.com>
2024-04-14 00:40:16 +0900

    initに追加手法を追加

--- src/optimizers/__init__.py
+++ src/optimizers/__init__.py
@@ -1,7 +1,9 @@
 # ruff: noqa: F403
 from ._factory import create_optimizer
 from ._registry import list_optimizers
+from .expo_fair import *
 from .greedy import *
+from .greedy_nsw import *
 from .nsw import *
 from .ot_osw import *
 
