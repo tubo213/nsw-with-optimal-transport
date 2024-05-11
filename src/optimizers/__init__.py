f8d0e1990003973086e3bf011add8674c7a07b1a
tubo213 <yuuki.32040808@gmail.com>
2024-05-11 21:06:47 +0900

    feat: Add PGD-NSW and OT-NSW optimizers

--- src/optimizers/__init__.py
+++ src/optimizers/__init__.py
@@ -5,6 +5,7 @@ from .expo_fair import *
 from .greedy import *
 from .greedy_nsw import *
 from .nsw import *
-from .ot_osw import *
+from .ot_nsw import *
+from .pgd_nsw import *
 
 __all__ = ["create_optimizer", "list_optimizers"]
